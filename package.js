23f9747e7229f7c6d54d5238653d61abd68e2b77
Andrey Maksimov <meamka@ya.ru>
2017-11-01 17:53:20 +0300

    Version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'meamka:useraccounts-spectre',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Accounts Templates styled with Spectre.css',
   // URL to the Git repository containing the source code for this package.
